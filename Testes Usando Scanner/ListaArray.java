23f28c51a1d9b6a78f21b3dec75e9405aa704e8a
Victoria Lacerda <victoria.simeao@soulasalle.com.br>
2025-03-17 20:35:27 -0300

    Create ListaArray.java

---
+++ Testes Usando Scanner/ListaArray.java
@@ -0,0 +1,36 @@
+class ListaArray {
+    private int[] array;
+    private int tamanho;
+
+    public ListaArray(int capacidade) {
+        this.array = new int[capacidade];
+        this.tamanho = 0;
+    }
+
+    public void adicionar(int elemento) {
+        if (tamanho == array.length) {
+            System.out.println("Erro: Lista cheia!");
+            return;
+        }
+        array[tamanho++] = elemento;
+    }
+
+    public void remover(int posicao) {
+        if (posicao < 0 || posicao >= tamanho) {
+            System.out.println("Erro: Posição inválida!");
+            return;
+        }
+        for (int i = posicao; i < tamanho - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        tamanho--;
+    }
+
+    public void imprimirLista() {
+        System.out.print("Lista: ");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
