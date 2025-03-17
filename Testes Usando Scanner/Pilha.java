c5d65b2a0eca075d97faca1502d462455c6faea8
Victoria Lacerda <victoria.simeao@soulasalle.com.br>
2025-03-17 20:36:37 -0300

    Create Pilha.java

---
+++ Testes Usando Scanner/Pilha.java
@@ -0,0 +1,51 @@
+class Pilha {
+    private int topo;
+    private int[] elementos;
+    private int capacidade;
+
+    public Pilha(int capacidade) {
+        this.capacidade = capacidade;
+        this.elementos = new int[capacidade];
+        this.topo = -1; // Indica pilha vazia
+    }
+
+    public boolean isEmpty() {
+        return topo == -1;
+    }
+
+    public boolean isFull() {
+        return topo == capacidade - 1;
+    }
+
+    public void push(int elemento) {
+        if (isFull()) {
+            System.out.println("Erro: Pilha cheia!");
+            return;
+        }
+        elementos[++topo] = elemento;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Erro: Pilha vazia!");
+            return -1;
+        }
+        return elementos[topo--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Erro: Pilha vazia!");
+            return -1;
+        }
+        return elementos[topo];
+    }
+
+    public void imprimirPilha() {
+        System.out.print("Pilha: ");
+        for (int i = 0; i <= topo; i++) {
+            System.out.print(elementos[i] + " ");
+        }
+        System.out.println();
+    }
+}
